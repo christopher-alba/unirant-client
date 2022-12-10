81a3b982a8c89e3f87f6132178159c0c0a88e4bd
Christopher Alba <chris.alba.dev@gmail.com>
2022-12-10 18:52:23 +1300

    remove slash from url

--- src/api/origin.ts
+++ src/api/origin.ts
@@ -1,4 +1,4 @@
 export const originURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://unirant-server.herokuapp.com/";
+    : "https://unirant-server.herokuapp.com";
